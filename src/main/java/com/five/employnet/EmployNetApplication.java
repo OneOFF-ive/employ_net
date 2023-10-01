3b84d8db680152d65509004a61fcc06b7e7f8de7
Wu <2321141691@qq.com>
2023-10-01 15:42:16 +0800

    配置https

--- src/main/java/com/five/employnet/EmployNetApplication.java
+++ src/main/java/com/five/employnet/EmployNetApplication.java
@@ -1,10 +1,16 @@
 package com.five.employnet;
 
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.naming.Context;
 
 @SpringBootApplication
 @MapperScan("com.five.employnet.mapper")
