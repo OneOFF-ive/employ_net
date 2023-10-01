aa1fdfa6f1cf405ef96f2741764d0c094494f171
Wu <2321141691@qq.com>
2023-10-01 23:35:20 +0800

    优化import

--- src/main/java/com/five/employnet/controller/UserController.java
+++ src/main/java/com/five/employnet/controller/UserController.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.five.employnet.common.BaseContext;
 import com.five.employnet.common.JwtUtil;
 import com.five.employnet.common.R;
-import com.five.employnet.dto.AdminDto;
 import com.five.employnet.dto.UserDto;
 import com.five.employnet.entity.*;
 import com.five.employnet.service.*;
