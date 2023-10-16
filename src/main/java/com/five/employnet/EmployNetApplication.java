0af916ecbcfdbe7e50f2b0b3170d01920bd9aacb
Wu <2321141691@qq.com>
2023-10-16 13:56:33 +0800

    启用websocket

--- src/main/java/com/five/employnet/EmployNetApplication.java
+++ src/main/java/com/five/employnet/EmployNetApplication.java
@@ -5,19 +5,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 
 @SpringBootApplication
 @MapperScan("com.five.employnet.mapper")
 @EnableTransactionManagement
 @EnableCaching
-@EnableWebSocket
 public class EmployNetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployNetApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(EmployNetApplication.class, args);
     }
+
 
 }
