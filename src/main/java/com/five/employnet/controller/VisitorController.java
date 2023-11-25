440ec32b30a35d1a8dd819a9b1574fea7b7120b6
Wu <2321141691@qq.com>
2023-11-25 13:08:53 +0800

    完善功能

--- src/main/java/com/five/employnet/controller/VisitorController.java
+++ src/main/java/com/five/employnet/controller/VisitorController.java
@@ -1,14 +1,20 @@
 package com.five.employnet.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.five.employnet.common.R;
 import com.five.employnet.config.CorsConfig;
+import com.five.employnet.entity.Visitor;
 import com.five.employnet.service.VisitorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
 @RestController
 @Slf4j
-@RequestMapping("/notice")
+@RequestMapping("/visitor")
 @CrossOrigin(origins = CorsConfig.origins, allowCredentials = "true")
 public class VisitorController {
     final private VisitorService visitorService;
@@ -17,15 +23,24 @@ public class VisitorController {
         this.visitorService = visitorService;
     }
 
-    @GetMapping("/total")
+    @GetMapping("/view/total")
     R<Integer> total() {
         int total = visitorService.count();
         return R.success(total);
     }
 
-//    @GetMapping("/{date}")
-//    R<Integer> visitsAt(@PathVariable String date) {
-//
-//    }
+    @GetMapping("/view/{date}")
+    R<Integer> visitsAt(@PathVariable String date) {
+        QueryWrapper<Visitor> queryWrapper = new QueryWrapper<>();
+        queryWrapper
+                .select("count(*) as count")
+                .groupBy("date").having("date = {0}", date);
+        Map<String, Object> res = visitorService.getMap(queryWrapper);
+        if (res != null) {
+            int count = (int) res.get("count");
+            return R.success(count);
+        }
+        return R.success(0);
+    }
 
 }
